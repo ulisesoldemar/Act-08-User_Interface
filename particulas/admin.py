ce3c0ac0badb64dccbbfdc8f35a879daf4d7b920
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-10-18 15:34:28 -0500

    Clase administradora de particulas y clase Partícula

---
+++ particulas/admin.py
@@ -0,0 +1,20 @@
+from .particula import Particula
+
+class Admin:
+    def __init__(self):
+        self.__particulas = []
+
+    def agregar_inicio(self, particula:Particula):
+        self.__particulas.insert(0, particula)
+
+    def agregar_final(self, particula:Particula):
+        self.__particulas.append(particula)
+
+    def mostar(self):
+        for particula in self.__particulas:
+            print(particula)
+    
+    def __str__(self):
+        return "".join(
+            str(particula) + '\n' for particula in self.__particulas
+        )
