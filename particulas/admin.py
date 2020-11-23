59981345d6685c938abaf07c4663197fcfece731
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-11-22 18:04:06 -0600

    Agregada la creacion de grafos

--- particulas/admin.py
+++ particulas/admin.py
@@ -4,6 +4,7 @@ import json
 class Admin:
     def __init__(self):
         self.__particulas = []
+        self.__grafo = dict()
 
     def agregar_inicio(self, particula:Particula):
         self.__particulas.insert(0, particula)
@@ -60,3 +61,23 @@ class Admin:
     
     def ordenar(self, by, reversal:bool=False):
         self.__particulas.sort(key=by, reverse=reversal)
+    
+    def grafo(self):
+        self.__grafo.clear()
+        for particula in self.__particulas:
+            origen = (particula.origen_x, particula.origen_y)
+            destino = (particula.destino_x, particula.destino_y)
+            arista_o_d = (destino, particula.distancia)
+            arista_d_o = (origen, particula.distancia)
+            
+            if origen in self.__grafo:
+                self.__grafo[origen].append(arista_o_d)
+            else:
+                self.__grafo[origen] = [arista_o_d]
+            if destino in self.__grafo:
+                self.__grafo[destino].append(arista_d_o)
+            else:
+                self.__grafo[destino] = [arista_d_o]
+        return self.__grafo
+
+        
